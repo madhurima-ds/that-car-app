9d3d058debc678b17239c568e458cac2a10f66ee
chadgormley <chad.gormley@gmail.com>
2023-08-30 07:16:06 -0400

    I needed to fix the add inventory button and format files

--- src/Pages/Inventory.js
+++ src/Pages/Inventory.js
@@ -4,36 +4,50 @@ import InventoryOutput from "../components/InventoryOutput";
 import AddInventory from "../components/AddInventory/AddInventory";
 import Button from "../components/Button";
 
-const Inventory = (props) => {
+import classes from "../components/AddInventory/AddInventory.module.css";
 
+const Inventory = (props) => {
   const [displayAddVehicle, setDisplayAddVehicle] = useState(false);
 
-  const [inventory, setInventory] = useState(props.inventoryList);  
+  const [inventory, setInventory] = useState(props.inventoryList);
 
-  const addInventoryHandler = (newVehicle) => {     
-    // *** Todo: Look into why the props.onUpdate(...) doesn't work in this scenario       
-    //setInventory((prevList) => {        
-      //return [newVehicle, ...prevList];
+  const addInventoryHandler = (newVehicle) => {
+    // *** Todo: Look into why the props.onUpdate(...) doesn't work in this scenario
+    //setInventory((prevList) => {
+    //return [newVehicle, ...prevList];
     //});
     //props.onUpdate(inventory);
-    const newInventory  = [newVehicle, ...inventory];
+    const newInventory = [newVehicle, ...inventory];
     setInventory(newInventory);
     props.onUpdate(newInventory);
   };
 
   const showAddVehicleHandler = () => {
     setDisplayAddVehicle(true);
-  }
+  };
 
   const hideAddVehicleHandler = () => {
     setDisplayAddVehicle(false);
-  }
-
+  };
 
   return (
-    <React.Fragment>        
-      <Button onClick={showAddVehicleHandler}>Add</Button>
-      {displayAddVehicle && <AddInventory onSave={addInventoryHandler} onCancel={hideAddVehicleHandler}/>}
+    <React.Fragment>
+      {!displayAddVehicle && (
+        <div className={classes.new_vehicle}>
+          <button
+            className={classes.new_vehicle}
+            onClick={showAddVehicleHandler}
+          >
+            Add Vehicle
+          </button>
+        </div>
+      )}
+      {displayAddVehicle && (
+        <AddInventory
+          onSave={addInventoryHandler}
+          onCancel={hideAddVehicleHandler}
+        />
+      )}
       <div className="row">
         <div className="main">
           {props.inventoryList && (
